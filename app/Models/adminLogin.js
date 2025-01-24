68928cab2b68b3cec38bb0754cb12539bd8e9656
Mirza Zain <168978177+mzain4321@users.noreply.github.com>
2025-01-24 14:11:15 +0500

    updated userlogin and admin login schema

--- app/Models/adminLogin.js
+++ app/Models/adminLogin.js
@@ -1,8 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 
 const adminSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: true},
+    name: String,
+    email: { type: String, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
 },
 {
     timestamps: true,
--- app/Models/userLogin.js
+++ app/Models/userLogin.js
@@ -1,14 +1,16 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, select: true },
-  role: { type: String, required: true, select: true },
-},
-{
-  timestamps: true,
-});
+const userSchema = new Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Explicitly set the collection name
 userSchema.set("collection", "userLogin");
